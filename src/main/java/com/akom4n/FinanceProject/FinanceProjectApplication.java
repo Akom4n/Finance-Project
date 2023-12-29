7f92f3b0762eb4fcf9237a975b5a56ad0da2d0c3
Vinicius Silva <xdenik13@gmail.com>
2023-12-29 11:58:27 -0300

    Iniciando Projeto

---
+++ src/main/java/com/akom4n/FinanceProject/FinanceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akom4n.FinanceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceProjectApplication.class, args);
+	}
+
+}
